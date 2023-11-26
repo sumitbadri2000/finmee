18e2f3aa3401662405feb3e1af7d9866751b33b4
sumitbadri2000 <sumitbadri2000@gmail.com>
2023-11-26 19:50:29 +0530

    update_clone

--- ntwist/src/App.js
+++ ntwist/src/App.js
@@ -1,15 +1,27 @@
+import { Box, Flex } from "@chakra-ui/react";
 import "./App.css";
 import Footer from "./Components/Footer";
 import Middle from "./Components/Middle";
 import Navbar from "./Components/Navbar";
+import Top from "./Components/Top";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
+    <Flex flexDirection={"column"}>
+      <Box
+        bg={`url('https://ntwist.com/wp-content/uploads/2022/03/2021-02-oil-and-gas-digital-hero-e1648984580884.png')`}
+        backgroundSize="cover"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        w="100%"
+        h="100vh">
+        <Navbar />
+        <Top />
+      </Box>
+
       <Middle />
       <Footer />
-    </div>
+    </Flex>
   );
 }
 
--- ntwist/src/Components/Navbar.jsx
+++ ntwist/src/Components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Top from "./Top";
 import {
   Box,
   Flex,
@@ -9,6 +8,7 @@ import {
   useColorModeValue,
   Stack,
   HStack,
+  Text,
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
@@ -38,57 +38,55 @@ export default function Navbar() {
 
   return (
     <>
-      <Box
-        // className={style.back}
-        bg={`url('https://ntwist.com/wp-content/uploads/2022/03/2021-02-oil-and-gas-digital-hero-e1648984580884.png')`}
-        backgroundSize="cover"
-        backgroundPosition="center"
-        backgroundRepeat="no-repeat"
-        w="100%"
-        h="100vh">
-        <Box px={14}>
-          <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-            <Flex alignItems={"center"}>
-              <Image
-                w={{ base: "40%", sm: "50%", md: "35%", xl: "20%" }}
-                src="https://ntwist.com/wp-content/uploads/2021/12/ntwistlight.png"
-                alt=""
-              />
-            </Flex>
-            <HStack alignItems={"center"} spacing={6} width={"50%"}>
-              <HStack
-                spacing={6}
-                color={"white"}
-                as={"nav"}
-                fontSize={{ base: "14px", sm: "14px", md: "12px", xl: "17px" }}
-                fontWeight={"600"}
-                display={{ base: "none", md: "flex" }}>
-                {Links.map((link) => (
-                  <NavLink key={link}>{link}</NavLink>
-                ))}
-              </HStack>
-            </HStack>
-            <IconButton
-              size={"md"}
-              background={"transparent"}
-              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-              aria-label={"Open Menu"}
-              display={{ md: "none" }}
-              onClick={isOpen ? onClose : onOpen}
+      <Box px={14} position={"sticky"} zIndex={2} top={"0"}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Flex alignItems={"center"}>
+            <Image
+              w={{ base: "40%", sm: "50%", md: "35%", xl: "20%" }}
+              src="https://ntwist.com/wp-content/uploads/2021/12/ntwistlight.png"
+              alt=""
             />
           </Flex>
+          <HStack alignItems={"center"} spacing={6} width={"50%"}>
+            <HStack
+              spacing={6}
+              color={"white"}
+              as={"nav"}
+              fontSize={{ base: "12px", sm: "12px", md: "10px", xl: "15px" }}
+              fontWeight={"600"}
+              display={{ base: "none", md: "flex" }}>
+              {Links.map((link) => (
+                <Text
+                  _hover={{ borderBottom: "1px solid black" }}
+                  cursor={"pointer"}
+                  key={link}>
+                  {link}
+                </Text>
+              ))}
+            </HStack>
+          </HStack>
+          <IconButton
+            size={"md"}
+            background={"transparent"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+        </Flex>
 
-          {isOpen ? (
-            <Box pb={6} display={{ md: "none" }}>
-              <Stack as={"nav"} spacing={4} color={"white"}>
-                {Links.map((link) => (
-                  <NavLink key={link}>{link}</NavLink>
-                ))}
-              </Stack>
-            </Box>
-          ) : null}
-        </Box>
-        <Top />
+        {isOpen ? (
+          <Box pb={6} display={{ md: "none" }}>
+            <Stack
+              spacing={4}
+              color={"white"}
+              _hover={{ borderBottom: "1px solid black" }}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
       </Box>
     </>
   );
